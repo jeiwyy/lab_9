4393d18a15aa886e1f0d5234d71c442d061a3324
jeiwyy <jeiwy1333@gmail.com>
2026-05-14 21:33:01 +0300

    add all files ¯\_(ツ)_/¯

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,167 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab_9
+{
+    public partial class MainWindow : Window
+    {
+        private RightTriangle tri1;
+        private RightTriangle tri2;
+        public MainWindow()
+        {
+            InitializeComponent();
+            tri1 = new RightTriangle();
+            tri2 = new RightTriangle();
+        }
+
+        private void SideA_Changed(object sender, TextChangedEventArgs e)
+        {
+            double value;
+            if (double.TryParse(SideA.Text, out value) && value > 0)
+            {
+                tri1.A = value;
+                if (tri1)
+                {
+                    Tri1.Text = "Площадь 1\nтреугольника:\n" + (double)tri1;
+                    return;
+                }
+            }
+            Tri1.Text = "Треугольник не существует!";
+        }
+
+        private void SideB_Changed(object sender, TextChangedEventArgs e)
+        {
+            double value;
+            if (double.TryParse(SideB.Text, out value) && value > 0)
+            {
+                tri1.B = value;
+                if (tri1)
+                {
+                    Tri1.Text = "Площадь 1\nтреугольника:\n" + (double)tri1;
+                    return;
+                }
+            } 
+            Tri1.Text = "Треугольник не существует!";
+        }
+
+        private void SideA2_Changed(object sender, TextChangedEventArgs e)
+        {
+            double value;
+            if (double.TryParse(SideA_2.Text, out value) && value > 0)
+            {
+                tri2.A = value;
+                if (tri2)
+                {
+                    Tri2.Text = "Площадь 2\nтреугольника:\n" + tri2.CalcArea();
+                    return;
+                }
+            }
+            Tri2.Text = "Треугольник не существует!";
+        }
+
+        private void SideB2_Changed(object sender, TextChangedEventArgs e)
+        {
+            double value;
+            if (double.TryParse(SideB_2.Text, out value) && value > 0)
+            {
+                tri2.B = value;
+                if (tri2)
+                {
+                    Tri2.Text = "Площадь 2\nтреугольника:\n" + tri2.CalcArea();
+                    return;
+                }
+            }
+            Tri2.Text = "Треугольник не существует!";
+        }
+
+        private void Plus_1_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Tri1.Text.EndsWith('!') && tri1)
+            {
+                tri1++;
+                Tri1.Text = "Площадь 1\nтреугольника:\n" + (double)tri1;
+                SideA.Text = tri1.A.ToString();
+                SideB.Text = tri1.B.ToString();
+            }
+            else
+            {
+                Status.Text = "Треугольник 1 не существует!";
+            }
+        }
+
+        private void Minus_1_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Tri1.Text.EndsWith('!') && tri1)
+            {
+                tri1--;
+                Tri1.Text = "Площадь 1\nтреугольника:\n" + (double)tri1;
+                SideA.Text = tri1.A.ToString();
+                SideB.Text = tri1.B.ToString();
+            }
+            else
+            {
+                Status.Text = "Треугольник 1 не существует!";
+            }
+        }
+
+        private void Plus_2_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Tri2.Text.EndsWith('!') && tri2)
+            {
+                tri2++;
+                Tri2.Text = "Площадь 2\nтреугольника:\n" + tri2.CalcArea();
+                SideA_2.Text = tri2.A.ToString();
+                SideB_2.Text = tri2.B.ToString();
+            }
+            else
+            {
+                Status.Text = "Треугольник 2 не существует!";
+            }
+        }
+
+        private void Minus_2_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Tri2.Text.EndsWith('!') && tri2)
+            {
+                tri2--;
+                Tri2.Text = "Площадь 2\nтреугольника:\n" + tri2.CalcArea();
+                SideA_2.Text = tri2.A.ToString();
+                SideB_2.Text = tri2.B.ToString();
+            }
+            else
+            {
+                Status.Text = "Треугольник 2 не существует!";
+            }
+        }
+
+        private void Compare_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Tri2.Text.EndsWith('!') && !Tri1.Text.EndsWith('!')
+                && tri1 && tri2)
+            {
+                if (tri1 >= tri2)
+                {
+                    Status.Text = "Площадь первого треугольника больше" +
+                        " или равна второму";
+                }
+                else
+                {
+                    Status.Text = "Площадь второго треугольника больше" +
+                        " первого";
+                }
+            }
+            else
+            {
+                Status.Text = "Один из треугольников не существует";
+            }
+        }
+    }
+}
---
+++ RightTriangle.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab_9;
+internal class RightTriangle
+{
+    private double _a;
+    private double _b;
+
+    public RightTriangle()
+    {
+        _a = 0;
+        _b = 0;
+    }
+
+    public RightTriangle(double _a, double _b)
+    {
+        if (_a > 0 && _b > 0)
+        {
+            this._a = _a;
+            this._b = _b;
+        }
+        else
+        {
+            Console.WriteLine("Ошибка ввода!");
+        }
+    }
+
+    public double A
+    {
+        get
+        {
+            return _a;
+        }
+        set
+        {
+            this._a = value;
+        }
+    }
+    public double B
+    {
+        get
+        {
+            return _b;
+        }
+        set
+        {
+            this._b = value;
+        }
+    }
+
+    public double CalcArea()
+    {
+        if (_a > 0 && _b > 0)
+        {
+            return _a * _b / 2;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    public override string ToString()
+    {
+        return "a = " + _a + " b = " + _b;
+    }
+
+    public static RightTriangle operator ++(RightTriangle triangle)
+    {
+        return new RightTriangle(triangle._a * 2, triangle._b * 2);
+    }
+
+    public static RightTriangle operator --(RightTriangle triangle)
+    {
+        return new RightTriangle(triangle._a / 2, triangle._b / 2);
+    }
+
+    public static explicit operator double(RightTriangle triangle)
+    {
+        if (triangle._a > 0 && triangle._b > 0)
+        {
+            return triangle.CalcArea();
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
+    public static implicit operator bool(RightTriangle triangle)
+    {
+        if (triangle._a > 0 && triangle._b > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static bool operator <=(RightTriangle a, RightTriangle b)
+    {
+        return a.CalcArea() <= b.CalcArea();
+    }
+
+    public static bool operator >=(RightTriangle a, RightTriangle b)
+    {
+        return a.CalcArea() >= b.CalcArea();
+    }
+}
